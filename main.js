bbdf23c042a8c06727834ca8bd43a2b3bd110cc8
shubhu003009 <shubhamjangid990@gmail.com>
2025-01-10 13:34:46 +0530

    LAYOUT | hero section

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const locomotiveScroll = new LocomotiveScroll();
 
+const toggleBtn = document.querySelector(".toggle-btn");
+const mobileNav = document.querySelector(".mobileNav");
+
 const body = document.body;
 let lastscroll = 0;
 
@@ -12,16 +15,21 @@ window.addEventListener("scroll", () => {
   if (currentScroll > 50 && !body.classList.contains("scroll-down")) {
     body.classList.remove("scroll-up");
     body.classList.add("scroll-down");
+    mobileNav.classList.remove("active");
+    toggleBtn.classList.remove("active");
   }
   if (currentScroll < lastscroll && body.classList.contains("scroll-down")) {
     body.classList.remove("scroll-down");
     body.classList.add("scroll-up");
     body.classList.add("nav-scroll-color");
   }
-
   if (currentScroll < 50) {
     body.classList.remove("nav-scroll-color");
   }
-
   lastscroll = currentScroll;
 });
+
+const toggleMobileNav = () => {
+  mobileNav.classList.toggle("active");
+  toggleBtn.classList.toggle("active");
+};
